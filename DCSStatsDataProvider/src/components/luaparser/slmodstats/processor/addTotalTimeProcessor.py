d7abdec8145878871e64c6f0d6cd9a32dd102f64
Elias Cecetka <Elias_2000@web.de>
2023-04-26 20:40:18 +0200

    error checking

--- DCSStatsDataProvider/src/components/luaparser/slmodstats/processor/addTotalTimeProcessor.py
+++ DCSStatsDataProvider/src/components/luaparser/slmodstats/processor/addTotalTimeProcessor.py
@@ -1,11 +1,13 @@
 from src.components.luaparser.slmodstats.processor.updateLuaDecoded import recursive_dict_merge
 
-
 def addTotalTime(luadecoded):
     for ucid in luadecoded:
         totalTime = 0
         for aircrafttimes in luadecoded[ucid]["times"]:
-            totalTime+=luadecoded[ucid]["times"][aircrafttimes]["total"]
+            try:
+                totalTime += luadecoded[ucid]["times"][aircrafttimes]["total"]
+            except KeyError:
+                continue
         luadecoded[ucid]["times"]["totalFlightTime"] = totalTime
 
     return luadecoded
