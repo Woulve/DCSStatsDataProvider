c77273869e717392a7c7805bfd1ee3605bac1d69
Elias Cecetka <elias_2000@web.de>
2023-07-21 10:21:27 +0200

    Logging handler

--- DCSStatsDataProvider/src/util/serverlogger.py
+++ DCSStatsDataProvider/src/util/serverlogger.py
@@ -4,10 +4,9 @@ import sys
 
 def serverLogger():
     logFile = 'serverlog.log'
-    # my_handler =
     logging.basicConfig(
         handlers=[
-                  RotatingFileHandler(logFile, mode='a', maxBytes=8*1024, backupCount=3, encoding=None, delay=0),
+                  RotatingFileHandler(logFile, mode='a', maxBytes=1000000, backupCount=0, encoding=None, delay=True),
                   logging.StreamHandler(sys.stdout),
                   ],
         format='%(levelname)s:     %(asctime)s.%(msecs)d %(funcName)s:%(lineno)d %(name)s %(message)s',datefmt='%Y-%m-%d %H:%M:%S',
